2e6f89a79786a3c4938b3a88e69282305b8a7224
Solomon Eseme <kaperskyguru@gmail.com>
2021-07-19 12:40:57 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0e05437afe12f09808d4ae535476124b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23571009-31ac-4057-8905-aafcbeb1f128'
+};
